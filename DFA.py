b6e62624a66da166ad49a06405b5611efc3adf6c
antaylor6 <113199563+antaylor6@users.noreply.github.com>
2025-04-27 11:20:39 -0500

    Update DFA.py
    Added edge checking cases and reading user input

--- DFA.py
+++ DFA.py
@@ -1,18 +1,87 @@
-dfa1 = {
-    '0': {'a': '1', 'b': '0'},
-    '1': {'a': '0', 'b': '1'}
-}
-start1 = '0'
-accepts1 = {'1'}
+import sys
+dfaFile1 = sys.argv[1]
+dfaFile2 = sys.argv[2]
 
-dfa2 = {
-    '0':{'a': '1', 'b': '0'},
-    '1':{'a': '1', 'b': '2'},
-    '2':{'a': '2', 'b': '0'}
+# file format:
+# alphabet (separated by spaces)
+# state names (separated by spaces)
+# accepting states (separated by spaces)
+# input outputting state (in pairs, separated by spaces. same order as states specified)
+# example files are attached
+# EX:
+# 0 1
+# S1 S2
+# S1
+# 0 S1 1 S2 (this would map to S1)
+# 0 S2 1 S1 (this would map to S2)
 
-}
-start2 = '0'
-accepts2 = {'0','1'}
+
+dfa1 = {}
+with open(dfaFile1) as f:
+    alphabetLine = f.readline()
+    alphabet = alphabetLine.split()
+    states = f.readline()
+    statesList = states.split()
+    stateNames = statesList.copy()
+    acceptingState1 = f.readline()
+    accepts1 = acceptingState1.split()
+    for x in range(0, len(statesList)):
+        line = f.readline()
+        currentState = line.split()
+        statesList[x] = {}
+        if len(currentState) % 2 != 0:
+            print("ERROR! States are declared incorrectly for", dfaFile1, ". Please check comments for correct formatting.")
+            sys.exit()
+        for y in range(0, len(currentState),2):
+                if currentState[y] not in alphabet:
+                     print("ERROR!", currentState[y], "is not part of the specified alphabet. Please edit file ", dfaFile1, " for adjustments.")
+                     sys.exit()
+                if currentState[(y+1)] not in stateNames:
+                     print("ERROR!", currentState[y+1], "is not part of the specified states. Please edit file ", dfaFile1, " for adjustments.")
+                     sys.exit()
+                statesList[x][currentState[y]] = currentState[(y + 1)]
+        dfa1[stateNames[x]] = statesList[x]
+
+
+
+
+
+dfa2 = {}
+with open(dfaFile2) as f:
+    alphabetLine2 = f.readline()
+    alphabet2 = alphabetLine2.split()
+    states2 = f.readline()
+    statesList2 = states2.split()
+    stateNames2 = statesList2.copy()
+    acceptingState2 = f.readline()
+    accepts2 = acceptingState2.split()
+    for x in range(0, len(statesList2)):
+        line = f.readline()
+        currentState2 = line.split()
+        # make a dictionary for each state first
+        statesList2[x] = {}
+        if len(currentState2) % 2 != 0:
+            print("ERROR! States are declared incorrectly for", dfaFile2, ". Please check comments for correct formatting.")
+            sys.exit()
+        for y in range(0, len(currentState2),2):
+                if currentState2[y] not in alphabet2:
+                     print("ERROR!", currentState2[y], "is not part of the specified alphabet. Please edit file ", dfaFile2, " for adjustments.")
+                     sys.exit()
+                if currentState2[(y+1)] not in stateNames2:
+                     print("ERROR!", currentState2[y+1], "is not part of the specified states. Please edit file ", dfaFile2, " for adjustments.")
+                     sys.exit()
+                statesList2[x][currentState2[y]] = currentState2[(y + 1)]
+        dfa2[stateNames2[x]] = statesList2[x]
+
+print("DFA1:")
+print(dfa1)
+print("DFA2:")
+print(dfa2)
+print("\n")
+
+
+start1 = stateNames[0]
+start2 = stateNames2[0]
 
 def productConstruction(trans1, start1, accepts1, trans2, start2, accepts2):
     product_trans = {}
